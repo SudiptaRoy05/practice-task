23cb965bedab1d616a640b08afe6a20678d4d561
SudiptaRoy05 <sudiptaroyballave121@gmail.com>
2024-09-22 01:20:43 +0600

    dom added complete

--- script.js
+++ script.js
@@ -5,16 +5,21 @@ const totalPriceEl = document.getElementById('totalPrice');
 const cuponFieldEl = document.getElementById('cuponField');
 const cuponBtnEl = document.getElementById('cuponBtn');
 const defaultTextEl = document.getElementById('defaultText');
+const grandTotalEl = document.getElementById('grandTotal');
+const cuponAreaEl = document.getElementById('cuponArea');
+const phoneNumEl = document.getElementById('phoneNum');
+const nextBtnEl = document.getElementById('nextBtn');
 
+ 
 let selectedSeat = [];
 let totalPrice = 0;
 function handleSelectSeat(event) {
 
     if (selectedSeat.includes(event.innerText)) {
         return alert('seat already booked')
-    }else if(selectedSeat.length < 4){
+    } else if (selectedSeat.length < 4) {
 
-   
+
 
         event.classList.add('primary');
         event.classList.add('text-white');
@@ -51,8 +56,52 @@ function handleSelectSeat(event) {
                                 </tr>`
 
 
-    }else{
+    } else {
         return alert('maximum seat booked');
     }
 }
 
+
+let cuponSave = 0;
+document.getElementById('cuponBtn').addEventListener('click', function () {
+
+    const cpnInpValue = document.getElementById('cuponField').value;
+
+    console.log(cpnInpValue);
+    if (cpnInpValue !== 'CUPON50' && cpnInpValue !== 'Couple 20') {
+        return alert("Cupon is not valid")
+    } else {
+        if (cpnInpValue === 'CUPON50') {
+            cuponSave = totalPrice * .15;
+
+        } else if (cpnInpValue === 'Couple 20') {
+            cuponSave = totalPrice * .20;
+        }
+
+    }
+
+    cuponFieldEl.classList.add('hidden');
+    cuponBtnEl.classList.add('hidden');
+    cuponAreaEl.innerHTML = `<div class="flex justify-between text-lg font-semibold pt-5">
+                        <p>Discount Price :</p>
+                        <p>-BDT: ${cuponSave}</p>
+                    </div>`
+
+
+    const grandTotalValue = totalPrice - cuponSave;
+    grandTotalEl.innerText = grandTotalValue.toFixed(2);
+
+})
+
+
+phoneNumEl.addEventListener('input',function(event){
+    const inputValue = event.target.value;
+    if(inputValue.length >= 11){
+        nextBtnEl.removeAttribute('disabled')
+    }
+    
+})
+
+document.getElementById('btnContinue').addEventListener('click', function(){
+    window.location.reload();
+})
